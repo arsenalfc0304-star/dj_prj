da999f6e336e168cd1a1e964a783a4ef96d4a1a5
дмитрий рыбин <arsenalfc0304@gmail.com>
2026-05-09 13:30:28 +0500

    hw_22_started

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path, include
 from catalog.apps import CatalogConfig
+from catalog.views import home
 
 app_name = CatalogConfig.name
 urlpatterns = [
-    path('', )
+    path('', home, name='home'),
 ]

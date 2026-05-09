12a0e932df06e9e97d28a8a29622f6726a4b0744
дмитрий рыбин <arsenalfc0304@gmail.com>
2026-05-09 19:07:28 +0500

    hw_22_started

--- config/settings.py
+++ config/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql-psycopg2',
+        'NAME': 'django_project',
+        'USER': 'postgres',
+        'PASSWORD': '4815',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 

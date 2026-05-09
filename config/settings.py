d6868f67aada6d67b4bde8c93bb0ecd15070acdc
дмитрий рыбин <arsenalfc0304@gmail.com>
2026-05-09 19:38:46 +0500

    hw_23_started

--- config/settings.py
+++ config/settings.py
@@ -78,7 +78,7 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql-psycopg2',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': os.getenv('NAME'),
         'USER': os.getenv('USER'),
         'PASSWORD': os.getenv('PASSWORD'),
